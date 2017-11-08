1f45e2fa841610d49142f1343853874cb8f7d40e
yang.liu <lyxhsn@163.com>
2017-11-08 08:30:42 +0800

    Initial commit

---
+++ Assets/Editor/BuildBundle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class BuildBundle {
+
+    [MenuItem("LY/Build Asset")]
+    static void BuildAllAssertBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+
+}
---
+++ Assets/testInputField.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class testInputField : MonoBehaviour {
+
+    // Use this for initialization
+    InputField input;
+    void Start () {
+        input = gameObject.GetComponent<InputField>();
+        //input.ActivateInputField();
+        StartCoroutine("ToEnd", input);
+        //input.MoveTextEnd(false);
+        //input.caretPosition = input.text.Length;
+    }
+
+    IEnumerator ToEnd(InputField input)
+    {
+        yield return new WaitForSeconds(0.5f);
+        input.MoveTextEnd(false);
+    }
+
+    public void OnValueChange(string str)
+    {
+        Debug.LogError(str);
+    }
+
+    public void OnEndEdit(string str)
+    {
+        input.lineType = InputField.LineType.MultiLineSubmit;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
